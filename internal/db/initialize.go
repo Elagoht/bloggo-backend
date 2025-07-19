482d8e26eccc27ac5832b0bdfaf122cdb11f77d0
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-07-19 23:31:07 +0300

    chore: remove verbosity of database initialize panic

--- internal/db/initialize.go
+++ internal/db/initialize.go
@@ -2,14 +2,13 @@ package db
 
 import (
 	"database/sql"
-	"fmt"
 )
 
 func InitializeTables(database *sql.DB) {
 	for _, query := range InitializeQueries {
 		_, err := database.Exec(query)
 		if err != nil {
-			panic(fmt.Errorf("Database cannot be initialized: %w", err))
+			panic("Database cannot be initialized.")
 		}
 	}
 }
