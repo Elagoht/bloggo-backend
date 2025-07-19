9482653e23dd325eb99bc1bc53df34bb782440ab
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-07-19 14:07:03 +0300

    refactor: simplify main function by delegating server setup to application bootstrap

--- cli/main.go
+++ cli/main.go
@@ -1,35 +1,8 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/go-chi/chi"
-	"github.com/joho/godotenv"
-)
+import "bloggo/internal/app"
 
 func main() {
-	// Load .env file
-	if err := godotenv.Load(); err != nil {
-		log.Printf("No .env file found or failed to load: %v", err)
-	}
-
-	router := chi.NewRouter()
-
-	// Create server
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatalf("PORT environment variable is not set")
-	}
-
-	// Start the server
-	server := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
-	}
-	log.Printf("Starting server on http://localhost:%s", port)
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
+	application := app.GetInstance()
+	application.Bootstrap()
 }
