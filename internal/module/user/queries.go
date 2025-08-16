e4967baeff561e251946b9ebc66c76f1895540ed
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-08-16 20:40:51 +0300

    fix(user): update SQL queries to use 'created_by' instead of 'user_id' for counting posts

--- internal/module/user/queries.go
+++ internal/module/user/queries.go
@@ -3,14 +3,14 @@ package user
 const (
 	QueryUserGetUserCards = `
 	SELECT users.id, users.name, users.email, users.avatar, users.role_id, roles.name AS role_name,
-  (SELECT COUNT(*) FROM posts WHERE user_id = users.id AND deleted_at IS NULL) AS writtenPostCount,
-  (SELECT COUNT(*) FROM posts WHERE user_id = users.id AND deleted_at IS NULL AND published_version_id IS NOT NULL) AS publishedPostCount
+  (SELECT COUNT(*) FROM posts WHERE created_by = users.id AND deleted_at IS NULL) AS writtenPostCount,
+  (SELECT COUNT(*) FROM posts WHERE created_by = users.id AND deleted_at IS NULL AND current_version_id IS NOT NULL) AS publishedPostCount
 	FROM users
 	LEFT JOIN roles ON users.role_id = roles.id
 	WHERE users.deleted_at IS NULL%s;`
 	QueryUserGetById = `SELECT users.id, users.name, users.email, users.avatar, users.created_at, users.last_login, users.role_id, roles.name AS role_name,
-  (SELECT COUNT(*) FROM posts WHERE user_id = users.id AND deleted_at IS NULL) AS writtenPostCount,
-  (SELECT COUNT(*) FROM posts WHERE user_id = users.id AND deleted_at IS NULL AND published_version_id IS NOT NULL) AS publishedPostCount
+  (SELECT COUNT(*) FROM posts WHERE created_by = users.id AND deleted_at IS NULL) AS writtenPostCount,
+  (SELECT COUNT(*) FROM posts WHERE created_by = users.id AND deleted_at IS NULL AND current_version_id IS NOT NULL) AS publishedPostCount
 	FROM users
 	LEFT JOIN roles ON users.role_id = roles.id
 	WHERE users.id = ? AND users.deleted_at IS NULL;`
