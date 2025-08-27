9ac64da8738d24ad995868334952621eb06c09e5
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-08-27 22:31:34 +0300

    fix(user): update SQL queries to accurately count written posts by excluding deleted and unpublished versions

--- internal/module/user/queries.go
+++ internal/module/user/queries.go
@@ -3,7 +3,7 @@ package user
 const (
 	QueryUserGetUserCards = `
 	SELECT users.id, users.name, users.email, users.avatar, users.role_id, roles.name AS role_name,
-  (SELECT COUNT(*) FROM posts WHERE created_by = users.id AND deleted_at IS NULL) AS writtenPostCount,
+  (SELECT COUNT(*) FROM posts p JOIN post_versions pv ON p.current_version_id = pv.id WHERE p.created_by = users.id AND p.deleted_at IS NULL AND pv.status != 0) AS writtenPostCount,
   (SELECT COUNT(*) FROM posts p JOIN post_versions pv ON p.current_version_id = pv.id WHERE p.created_by = users.id AND p.deleted_at IS NULL AND pv.status = 5) AS publishedPostCount
 	FROM users
 	LEFT JOIN roles ON users.role_id = roles.id
@@ -13,7 +13,7 @@ const (
 	FROM users
 	WHERE users.deleted_at IS NULL%s;`
 	QueryUserGetById = `SELECT users.id, users.name, users.email, users.avatar, users.created_at, users.last_login, users.role_id, roles.name AS role_name,
-  (SELECT COUNT(*) FROM posts WHERE created_by = users.id AND deleted_at IS NULL) AS writtenPostCount,
+  (SELECT COUNT(*) FROM posts p JOIN post_versions pv ON p.current_version_id = pv.id WHERE p.created_by = users.id AND p.deleted_at IS NULL AND pv.status != 0) AS writtenPostCount,
   (SELECT COUNT(*) FROM posts p JOIN post_versions pv ON p.current_version_id = pv.id WHERE p.created_by = users.id AND p.deleted_at IS NULL AND pv.status = 5) AS publishedPostCount
 	FROM users
 	LEFT JOIN roles ON users.role_id = roles.id
